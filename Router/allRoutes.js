b41c0671fe379eba09c122bfc8be03282e3f5b5e
Riviera77 <103321314+Riviera77@users.noreply.github.com>
2024-09-12 21:50:30 +0200

    update

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,8 @@ export const allRoutes = [
 	new Route('/', 'Accueil', '/pages/home.html'),
 	new Route('/services', 'services', '/pages/services.html'),
 	new Route('/habitats', 'habitats', '/pages/habitats.html'),
+	new Route('/hippo', 'hippo', '/pages/hippo.html'),
+	new Route('/hippo-nain', 'hippo-nain', '/pages/hippo-nain.html'),
 	new Route('/contact', 'contact', '/pages/contact.html'),
 	new Route('/connexion', 'connexion', '/pages/connexion.html'),
 ];
