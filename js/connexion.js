48af15aebf9d8815a262bbf2bf22b5d51323111f
Riviera77 <103321314+Riviera77@users.noreply.github.com>
2024-11-04 12:39:58 +0100

    update connexion

--- js/connexion.js
+++ js/connexion.js
@@ -1,68 +1,19 @@
-// Implémenter le JS de ma page connexion
-// récupérer les éléments du DOM
-const inputUsername = document.getElementById('UsernameInput');
-const inputPassword = document.getElementById('PasswordInput');
-const submitButton = document.getElementById('btn-validation-connexion');
+// Description: Script pour la page de connexion
+const mailInput = document.getElementById('EmailInput');
+const passwordInput = document.getElementById('PasswordInput');
+const btnSignIn = document.getElementById('btnSignin');
 
-// Ajouter un écouteur d'événement sur les champs de saisie
-inputUsername.addEventListener('keyup', validateForm);
-inputPassword.addEventListener('keyup', validateForm);
+btnSignIn.addEventListener('click', checkCredentials);
 
-// Fonction de validation du formulaire
-function validateForm(params) {
-	const usernameOk = validateRequired(inputUsername);
-	const mailOk = validateMail(inputUsername);
-	const passwordOk = validatePassword(inputPassword);
-
-	if (usernameOk && passwordOk && mailOk) {
-		submitButton.disabled = false;
+function checkCredentials() {
+	// infos factices pour le test; il faudra appeler l'API pour vérifier les credentials en BDD
+	if (mailInput.value == 'test@mail.com' && passwordInput.value == 'Mimo@123') {
+		alert('Connexion réussie');
+		// Rediriger l'utilisateur vers la page d'accueil si connexion réussie
+		window.location.replace('/');
 	} else {
-		submitButton.disabled = true;
-	}
-}
-// validation de l'email avec création d'un regex
-function validateMail(input) {
-	//définir mon regex
-	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-	const mailUser = input.value;
-
-	if (mailUser.match(emailRegex)) {
-		input.classList.add('is-valid');
-		input.classList.remove('is-invalid');
-		return true;
-	} else {
-		input.classList.add('is-invalid');
-		input.classList.remove('is-valid');
-		return false;
-	}
-}
-
-// validation du mot de passe avec création d'un regex
-function validatePassword(input) {
-	//définir mon regex
-	const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
-	const passwordUser = input.value;
-
-	if (passwordUser.match(passwordRegex)) {
-		input.classList.add('is-valid');
-		input.classList.remove('is-invalid');
-		return true;
-	} else {
-		input.classList.add('is-invalid');
-		input.classList.remove('is-valid');
-		return false;
-	}
-}
-
-// Fonction de validation des champs requis
-function validateRequired(input) {
-	if (input.value === '') {
-		input.classList.add('is-invalid');
-		input.classList.remove('is-valid');
-		return false;
-	} else {
-		input.classList.add('is-valid');
-		input.classList.remove('is-invalid');
-		return true;
+		alert('Identifiants incorrects');
+		mailInput.classList.add('is-invalid');
+		passwordInput.classList.add('is-invalid');
 	}
 }
